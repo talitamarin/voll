3c3c92a63ba611a17e090242264317622b8ca688
talitamarin <128840773+talitamarin@users.noreply.github.com>
2023-09-11 11:29:01 -0300

    att

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,64 @@
 import React from 'react';
-import {VStack} from 'native-base'
+import {VStack, Image, Text, Box, FormControl, Input, Button, Link} from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import Logo from './assets/Logo.png'
+import { Titulo } from './componentes/Titulo';
 
 export default function Login() {
   return (
-    <VStack>
-      
+    <VStack flex={1} alignItems='center' p={5} justifyContent='center'>
+      <Image source={Logo} alt='Logo Voll' />
+      <Titulo>
+        Faça login em sua conta
+      </Titulo>
+      <Box>
+        <FormControl mt={3}>
+          <FormControl.Label>Email</FormControl.Label>
+          <Input 
+          placeholder='Insira seu endereço de e-mail'
+          size='lg'
+          w='100%'
+          borderRadius='lg'
+          bgColor='gray.100'
+          shadow={3}
+          />
+        </FormControl>
+      <FormControl mt={3}>
+          <FormControl.Label>Senha</FormControl.Label>
+          <Input 
+          placeholder='Insira sua senha'
+          size='lg'
+          w='100%'
+          borderRadius='lg'
+          bgColor='gray.100'
+          shadow={3}
+          />
+        </FormControl>
+        </Box>
+        <Button
+        w='100%'
+        bg='blue.800'
+        mt={10}
+        borderRadius='lg'
+        >
+          Entrar
+        </Button>
+        <Link href='https://www.alura.com.br' mt={2}>
+        Esqueceu sua senha ?
+        </Link>
+        <Box
+        w='100%'
+        flexDirection='row'
+        justifyContent='center'
+        mt={8}
+        >
+          <Text>
+            Ainda não tem cadastro ?
+            <TouchableOpacity>
+              <Text color='blue.500'>Faça seu cadastro!</Text>
+            </TouchableOpacity>
+          </Text>
+        </Box>
     </VStack>
   )
 }
---
+++ src/componentes/Titulo.tsx
@@ -0,0 +1,20 @@
+import { Text } from "native-base"
+
+interface TituloProps extends ITextProps {
+    children: ReactNode
+}
+
+export function Titulo({children, ...rest}: TituloProps) {
+    return (
+        <Text
+        fontSize='2xl'
+        fontWeight='bold'
+        color='gray.500'
+        textAlign='center'
+        mt={5}
+        {...rest}
+        >
+          {children}
+        </Text>
+    )
+}
