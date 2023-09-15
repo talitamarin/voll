12ce95719744eb61594d4173251e1b3a7407029e
talitamarin <128840773+talitamarin@users.noreply.github.com>
2023-09-15 16:01:28 -0300

    Att final

--- src/tabs/Consultas.tsx
+++ src/tabs/Consultas.tsx
@@ -1,10 +1,47 @@
-import { VStack, Text } from 'native-base'
+import { VStack, Divider , ScrollView} from 'native-base'
+import { CardConsulta } from '../componentes/CardConsulta'
+import { Botao } from '../componentes/Botao'
+import { Titulo } from '../componentes/Titulo'
 
 export default function Consultas(){
   return(
-  <VStack>
-    <Text> Consultas</Text>
-  </VStack>
+  <ScrollView p={5}>
+    <Titulo p={5} color='blue.500'> Minhas Consultas</Titulo>
+    <Botao mt={5} mb={5}>Agendar nova consulta</Botao>
+    <Titulo color='blue.500' fontSize='lg' alignSelf='flex-start'>Próximas Consultas</Titulo>
+    <CardConsulta 
+    nome='Dr. Felipe Matos'
+    especialidade='Ginecologista'
+    foto='https://github.com/FelipeMatosQA.png'
+    data='10/09/2023'
+    foiAgendado
+    />
+    <Divider mt={5} />
+
+    <Titulo color='blue.500' fontSize='lg' alignSelf='flex-start'>Consultas Passsadas</Titulo>
+    <CardConsulta 
+    nome='Dr. Felipe Matos'
+    especialidade='Ginecologista'
+    foto='https://github.com/FelipeMatosQA.png'
+    data='10/09/2023'
+    foiAtendido
+    />
+        <CardConsulta 
+    nome='Dr. Felipe Matos'
+    especialidade='Ginecologista'
+    foto='https://github.com/FelipeMatosQA.png'
+    data='10/09/2023'
+    foiAtendido
+    />
+        <CardConsulta 
+    nome='Dr. Felipe Matos'
+    especialidade='Ginecologista'
+    foto='https://github.com/FelipeMatosQA.png'
+    data='10/09/2023'
+    foiAtendido
+    />
+
+  </ScrollView>
 
   )
 }
--- src/tabs/Perfil.tsx
+++ src/tabs/Perfil.tsx
@@ -1,10 +1,26 @@
-import { VStack, Text } from 'native-base'
+import { VStack, Text, ScrollView, Avatar , Divider } from 'native-base'
+import {Titulo} from '../componentes/Titulo' ;
+
 
 export default function Perfil(){
   return(
-  <VStack>
-    <Text> Perfil</Text>
+    <ScrollView> 
+  <VStack flex={1} alignItems='center' p={5}>
+    <Titulo color='blue.500'>Meu Perfil</Titulo>
+    <Avatar size='xl' source={{uri: 'https://github.com/talitamarin.png'}}
+    mt={5}/>
+    <Titulo color='blue.500'>Informações Pessoais</Titulo>
+    <Titulo fontSize='lg'mb={1}>Talita Marin</Titulo>
+    <Text>23/09/1993</Text>
+    <Text>Recife-PE</Text>
+
+    <Divider mt={5}/>
+
+  <Titulo color='blue.500' mb={1}>Histórico Médico</Titulo>
+  <Text>Sinusite</Text>
+  <Text>Rinite</Text>
   </VStack>
+  </ScrollView>
 
   )
 }
